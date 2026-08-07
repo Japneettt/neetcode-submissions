a37a308c45cdff63e8e7c9aefc20d6bbf37b721f
Japneettt <149875996+Japneettt@users.noreply.github.com>
2026-08-08 02:07:48 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+ 
+      string genrateword(string s)
+      {
+        int count[26]={0};
+        for(char ch: s)
+        {
+            count[ch-'a']++;
+        }
+        string new_s="";
+        for(int i=0;i<26;i++)
+        {
+            if(count[i]>0)
+            {
+                new_s+=string(count[i],i+'a');
+            }
+        }
+        return new_s;
+      }
+
+
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mpp;
+        for(string &s :strs)
+        {
+            string new_s=genrateword(s);
+            mpp[new_s].push_back(s);
+        }
+        vector<vector<string>> result;
+        for(auto &it : mpp)
+        {
+            result.push_back(it.second);
+        }
+        return result;
+
+
+        
+    }
+};
